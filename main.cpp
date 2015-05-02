40f75a2fe201f518bbc4c2470b3b7ca496ea4189
TurtleHunter <turtlehunter@users.noreply.github.com>
2015-05-02 11:18:59 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include "mainwindow.h"
 #include <QApplication>
 
+#ifdef _WIN32
+    #define OS Windows
+#elif __APPLE__
+    #include "TargetConditionals.h"
+    #if TARGET_OS_MAC
+        #define OS Mac
+    #endif
+
+#elif __linux
+    #define OS Linux
+#elif __unix // all unices not caught above
+    #define OS Unix
+#elif __posix
+    #define OS POSIX
+#endif
+
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
