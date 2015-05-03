787e7d8fd28a4924360b27179db0d556b08578ea
Kamil Krzyżanowski <kamnxt@kamnxt.com>
2015-05-03 22:43:29 +0200

    Made FDB a class, moved init() into it and removed unrelated functions

--- fdb.cpp
+++ fdb.cpp
@@ -1,34 +1,23 @@
-#include <QSqlDatabase>
-#include <QSqlQuery>
+#include "fdb.h"
 
-void init() {
-    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+FDB::FDB(QObject *parent) : QObject(parent)
+{
+
+}
+
+bool FDB::init()
+{
+    db = QSqlDatabase::addDatabase("QSQLITE");
     db.setHostName("localhost");
     db.setDatabaseName("fusion.db");
     bool ok = db.open();
-    //ui->label->setText(ok ? "Connected!" : "Failed");
     if(!ok)
     {
-        //show a popup window / alert about the error
-        return;
+        // failed to open the db
+        return false;
     }
-    QSqlQuery query;
-    QSqlQuery queryPreferences;
-    QSqlQuery queryGames;
-
-    query.exec("CREATE TABLE IF NOT EXISTS prefs(key TINYTEXT NOT NULL, valuetype TINYINT NOT NULL, number TINYINT NOT NULL, text VARCHAR(255) NOT NULL);");
-    queryPreferences.exec("SELECT * FROM prefs;");
-    while(queryPreferences.next())
-    {
-        //get values here
-    }
-    //ui->label->setText(queryPreferences.value(1).toString());   for loop here?
-    //(later) if clientToken doesnt exists, show login and run registerClient(), if no account, //run register()
-    //(later) if lang is not set, set it to the default system language
-    //Then get game list
-    query.exec("CREATE TABLE IF NOT EXISTS games(gameName TEXT NOT NULL, gameType TINYINT NOT NULL , gameDirectory TEXT NOT NULL, relExecutablePath TEXT NOT NULL, PRIMARY KEY(gameName));");
-    queryGames.exec("SELECT * FROM games");
-    //TODO: get os and convert the full path  (Set by a #define OS)
-    //TODO: add everything to list
-
+    query = QSqlQuery(db);
+    query.exec("CREATE TABLE IF NOT EXISTS preferences (key TEXT NOT NULL, valuetype INTEGER NOT NULL, number INTEGER, text TEXT);");
+    query.exec("CREATE TABLE IF NOT EXISTS games (id INTEGER NOT NULL AUTO_INCREMENT, name TEXT NOT NULL, type INTEGER NOT NULL,");
+    return true;
 }
