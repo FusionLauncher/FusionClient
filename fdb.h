2678161987e9fd40435686df69d181c750a28388
Kamil Krzyżanowski <kamnxt@kamnxt.com>
2015-05-03 22:25:38 +0200

    Made FDB a class, moved init() into it and removed unrelated functions

--- fdb.h
+++ fdb.h
@@ -1,11 +1,24 @@
 #ifndef FDB_H
 #define FDB_H
 
+#include <QObject>
+#include <QSqlDatabase>
+#include <QSqlQuery>
 
-class FDB
+class FDB : public QObject
 {
+    Q_OBJECT
 public:
-    FDB();
+    explicit FDB(QObject *parent = 0);
+    bool init();
+
+private:
+    QSqlDatabase db;
+    QSqlQuery query;
+
+signals:
+
+public slots:
 };
 
 #endif // FDB_H
