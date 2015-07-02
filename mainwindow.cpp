d3720e49eda62340e91f71d093c3dda3f3f75a6c
Kamil Krzyżanowski <kamnxt@kamnxt.com>
2015-07-03 01:02:53 +0200

    Fixed the stylesheet button

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -38,6 +38,7 @@ MainWindow::MainWindow(QWidget *parent) :
     //required to store WindowSize on Resize
     resizeTimer.setSingleShot( true );
     connect( &resizeTimer, SIGNAL(timeout()), SLOT(resizeDone()) );
+    connect( ui->setStylesheetAction, SIGNAL(triggered()), this, SLOT(openStylesheetDialog()));
 }
 
 void MainWindow::reloadStylesheet()
@@ -283,11 +284,13 @@ void MainWindow::on_refreshUIAction_triggered()
     refreshList();
 }
 
-void MainWindow::on_setStylesheetAction_triggered()
+void MainWindow::openStylesheetDialog()
 {
     QString stylesheetFile = QFileDialog::getOpenFileName(this, "Choose stylesheet", QDir::homePath(), "*.qss");
-    if(QDir(stylesheetFile).exists())
+    qDebug() << "Stylesheet: " << stylesheetFile;
+    if(QFile::exists(stylesheetFile))
     {
+        qDebug() << "New stylesheet added: " << stylesheetFile;
         if(db.getTextPref("stylesheet").isNull())
         {
             db.addTextPref("stylesheet", stylesheetFile);
