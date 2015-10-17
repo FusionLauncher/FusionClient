d372dee42b835d449655d4aeb0bf4ddff06ba66c
Florian Bury <mail@florian-bury.de>
2015-10-17 16:29:27 +0200

    Add german language-files

---
+++ FusionLang_de.ts
@@ -0,0 +1,479 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>AddGameDialog</name>
+    <message>
+        <location filename="addgamedialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="addgamedialog.ui" line="30"/>
+        <source>Game name:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="addgamedialog.ui" line="51"/>
+        <source>Choose directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="addgamedialog.ui" line="64"/>
+        <source>No game directory set</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="addgamedialog.ui" line="94"/>
+        <source>Choose executable</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="addgamedialog.ui" line="104"/>
+        <source>No executable set</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="addgamedialog.ui" line="134"/>
+        <source>Open using:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="addgamedialog.ui" line="151"/>
+        <source>Browse...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="addgamedialog.ui" line="165"/>
+        <source>Arguments:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="addgamedialog.ui" line="184"/>
+        <source>Use launcher</source>
+        <translation type="unfinished">Benutze Launcher:</translation>
+    </message>
+</context>
+<context>
+    <name>AddLauncherDialog</name>
+    <message>
+        <source>Arguments</source>
+        <translation type="obsolete">Parameter</translation>
+    </message>
+    <message>
+        <source>Path</source>
+        <translation type="obsolete">Pfad</translation>
+    </message>
+    <message>
+        <source>Browse</source>
+        <translation type="obsolete">...</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation type="obsolete">Name</translation>
+    </message>
+</context>
+<context>
+    <name>EditLauncherDialog</name>
+    <message>
+        <source>Browse</source>
+        <translation type="obsolete">...</translation>
+    </message>
+</context>
+<context>
+    <name>FGameWidget</name>
+    <message>
+        <location filename="fgamewidget.ui" line="38"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fgamewidget.ui" line="122"/>
+        <source>No title found.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FSettingsDialog</name>
+    <message>
+        <location filename="fsettingsdialog.ui" line="17"/>
+        <source>Settings</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="fsettingsdialog.ui" line="97"/>
+        <source>Scan for new games on startup</source>
+        <translation>Suche neue Spiele beim Start</translation>
+    </message>
+    <message>
+        <location filename="fsettingsdialog.ui" line="123"/>
+        <source>Scan now</source>
+        <translation>Jetzt suchen</translation>
+    </message>
+    <message>
+        <location filename="fsettingsdialog.ui" line="133"/>
+        <source>soon.</source>
+        <translation>bald!</translation>
+    </message>
+    <message>
+        <location filename="fsettingsdialog.ui" line="139"/>
+        <source>Scan Now</source>
+        <translation>Jetzt suchen</translation>
+    </message>
+    <message>
+        <location filename="fsettingsdialog.ui" line="159"/>
+        <source>CurrentVersion</source>
+        <translation>AktuelleVersion</translation>
+    </message>
+    <message>
+        <location filename="fsettingsdialog.ui" line="169"/>
+        <source>Scan for Updates on Startup</source>
+        <translation>Suche nach updates beim Start</translation>
+    </message>
+    <message>
+        <location filename="fsettingsdialog.ui" line="189"/>
+        <source>Use Trayicon</source>
+        <translation>Zeige Trayicon</translation>
+    </message>
+    <message>
+        <location filename="fsettingsdialog.ui" line="209"/>
+        <source>Start with System</source>
+        <translation>Starte Fusion mit dem System</translation>
+    </message>
+    <message>
+        <location filename="fsettingsdialog.ui" line="261"/>
+        <source>Reset stylesheet</source>
+        <translation>Stylesheet zurücksetzten</translation>
+    </message>
+    <message>
+        <location filename="fsettingsdialog.ui" line="268"/>
+        <source>Load stylesheet</source>
+        <translation>Lade Stylesheet</translation>
+    </message>
+    <message>
+        <location filename="fsettingsdialog.ui" line="300"/>
+        <location filename="fsettingsdialog.ui" line="555"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="fsettingsdialog.ui" line="333"/>
+        <source>Cache artwork</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fsettingsdialog.ui" line="343"/>
+        <source>Clear cache</source>
+        <translation>Cache löschen</translation>
+    </message>
+    <message>
+        <location filename="fsettingsdialog.ui" line="350"/>
+        <source>Open cache folder</source>
+        <translation>Cache-Ordner öffnen</translation>
+    </message>
+    <message>
+        <location filename="fsettingsdialog.ui" line="370"/>
+        <source>Download all available artwork</source>
+        <translation>Alle Artwork suchen und herunterladen</translation>
+    </message>
+    <message>
+        <location filename="fsettingsdialog.ui" line="377"/>
+        <source>-</source>
+        <translation>-</translation>
+    </message>
+    <message>
+        <location filename="fsettingsdialog.ui" line="407"/>
+        <source>Add</source>
+        <translation>Hinzufügen</translation>
+    </message>
+    <message>
+        <location filename="fsettingsdialog.ui" line="414"/>
+        <location filename="fsettingsdialog.ui" line="498"/>
+        <source>Remove</source>
+        <translation>Entfernen</translation>
+    </message>
+    <message>
+        <location filename="fsettingsdialog.ui" line="421"/>
+        <source>Contains launchable ROMs</source>
+        <translation>Beinhaltet ROMs</translation>
+    </message>
+    <message>
+        <location filename="fsettingsdialog.ui" line="439"/>
+        <source>Name</source>
+        <translation>Name</translation>
+    </message>
+    <message>
+        <location filename="fsettingsdialog.ui" line="446"/>
+        <source>Browse</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="fsettingsdialog.ui" line="453"/>
+        <source>Arguments</source>
+        <translation>Parameter</translation>
+    </message>
+    <message>
+        <location filename="fsettingsdialog.ui" line="463"/>
+        <source>Path</source>
+        <translation>Pfad</translation>
+    </message>
+    <message>
+        <location filename="fsettingsdialog.ui" line="470"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;MS Shell Dlg 2&apos;; font-size:8.25pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt;&quot;&gt;Possible arguments:&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Sans Serif&apos;; font-size:9pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt;&quot;&gt;$GAMENAME	The game name&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt;&quot;&gt;$GAMEPATH	The path the game is launched from&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt;&quot;&gt;$GAMEEXE	The path to the executable, relative to $GAMEPATH&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fsettingsdialog.ui" line="488"/>
+        <source>Create</source>
+        <translation>Erstellen</translation>
+    </message>
+    <message>
+        <location filename="fsettingsdialog.ui" line="508"/>
+        <source>Compatible files:</source>
+        <translation>Dateiendungen:</translation>
+    </message>
+    <message>
+        <location filename="fsettingsdialog.ui" line="522"/>
+        <source>Local saved Backups:</source>
+        <translation>Anzahl Backups:</translation>
+    </message>
+    <message>
+        <location filename="fsettingsdialog.ui" line="542"/>
+        <source>Target-Dir</source>
+        <translation>Ziel-Verzeichnis</translation>
+    </message>
+    <message>
+        <location filename="fsettingsdialog.ui" line="577"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;MS Shell Dlg 2&apos;; font-size:8.25pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;To enable Syncing, select the Savegame-Folder in the Settings Dialog in each game, and a synced Directory here - for Example an Dropbox or OwnCloud folder.&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Fusion will then keep these Directorys up to date by overriding the old data.&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;You can specify how many Backups you want to keep locally saved.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>GameDBArtSelectorDialog</name>
+    <message>
+        <location filename="gamedbartselectordialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>GameInfoDialog</name>
+    <message>
+        <location filename="gameinfodialog.ui" line="14"/>
+        <source>Game Info</source>
+        <translation>Informationen</translation>
+    </message>
+    <message>
+        <location filename="gameinfodialog.ui" line="75"/>
+        <source>Type:</source>
+        <translation>Art:</translation>
+    </message>
+    <message>
+        <location filename="gameinfodialog.ui" line="85"/>
+        <source>Executable:</source>
+        <translation>Ausführbare Datei:</translation>
+    </message>
+    <message>
+        <location filename="gameinfodialog.ui" line="95"/>
+        <source>Directory:</source>
+        <translation>Verzeichnis:</translation>
+    </message>
+    <message>
+        <location filename="gameinfodialog.ui" line="105"/>
+        <source>Title:</source>
+        <translation>Name:</translation>
+    </message>
+    <message>
+        <location filename="gameinfodialog.ui" line="115"/>
+        <source>Parameters:</source>
+        <translation>Parameter:</translation>
+    </message>
+    <message>
+        <location filename="gameinfodialog.ui" line="143"/>
+        <location filename="gameinfodialog.ui" line="234"/>
+        <location filename="gameinfodialog.ui" line="311"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="gameinfodialog.ui" line="166"/>
+        <source>Use launcher</source>
+        <translation>Benutze Launcher:</translation>
+    </message>
+    <message>
+        <location filename="gameinfodialog.ui" line="256"/>
+        <source>Delete
+Game</source>
+        <translation>Spiel löschen</translation>
+    </message>
+    <message>
+        <location filename="gameinfodialog.ui" line="270"/>
+        <source>Sync Savegame</source>
+        <translation>Spielstände Synchronisieren</translation>
+    </message>
+    <message>
+        <location filename="gameinfodialog.ui" line="340"/>
+        <source>Artwork</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gameinfodialog.ui" line="361"/>
+        <source>Import Banner</source>
+        <translation>Banner importieren</translation>
+    </message>
+    <message>
+        <location filename="gameinfodialog.ui" line="393"/>
+        <source>Import Clearart</source>
+        <translation>Clearart importieren</translation>
+    </message>
+    <message>
+        <location filename="gameinfodialog.ui" line="415"/>
+        <source>Import Fanart</source>
+        <translation>Fanart importieren</translation>
+    </message>
+    <message>
+        <location filename="gameinfodialog.ui" line="425"/>
+        <source>Download Artwork</source>
+        <translation>Artwork herunterladen</translation>
+    </message>
+    <message>
+        <location filename="gameinfodialog.ui" line="571"/>
+        <source>Import Cover</source>
+        <translation>Cover importieren</translation>
+    </message>
+    <message>
+        <location filename="gameinfodialog.ui" line="581"/>
+        <source>Show Artwork-Folder</source>
+        <translation>Zeige Artwork-Ordner</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="26"/>
+        <source>FusionLauncher</source>
+        <translation>FusionLauncher</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="72"/>
+        <source>Project Fusion</source>
+        <translation type="unfinished">Fusion Launcher</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="156"/>
+        <source>STORE</source>
+        <translation type="unfinished">STORE</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="208"/>
+        <source>PushButton</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="232"/>
+        <source>LIBRARY</source>
+        <translation type="unfinished">LIBRARY</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="400"/>
+        <source>No games in List.</source>
+        <translation>Keine Spiele gefunden.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="447"/>
+        <source>lbl_lastPlayed</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="728"/>
+        <source>COMMUNITY</source>
+        <translation type="unfinished">COMMUNITY</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="752"/>
+        <source>Community - Coming soon!</source>
+        <translation>Community - kommt bald!</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="767"/>
+        <source>Reset database</source>
+        <translation>Datenbank zurücksetzten</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="772"/>
+        <source>Refresh UI</source>
+        <translation>Aktualisieren</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="777"/>
+        <source>Set stylesheet</source>
+        <translation>Setze Stylesheet</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="782"/>
+        <location filename="mainwindow.cpp" line="84"/>
+        <source>Add Game</source>
+        <extracomment>Entry for Settings-Menu</extracomment>
+        <translation>Spiel hinzufügen</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="787"/>
+        <source>Add Library</source>
+        <translation>Bibliothek hinzufügen</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="792"/>
+        <source>Add Origin-Game</source>
+        <translation>Origin-Spiel hinzufügen</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="797"/>
+        <source>Get Info</source>
+        <translation>Information</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="802"/>
+        <source>Switch View</source>
+        <translation>Wechsle Ansicht</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="807"/>
+        <source>Reset stylesheet</source>
+        <translation>Stylesheet zurücksetzten</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="82"/>
+        <source>Edit Game</source>
+        <extracomment>Entry for Settings-Menu</extracomment>
+        <translation>Spiel bearbeiten</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="86"/>
+        <source>Settings</source>
+        <extracomment>Entry for Settings-Menu</extracomment>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="88"/>
+        <source>Report Bug</source>
+        <extracomment>Entry for Settings-Menu</extracomment>
+        <translation>Fehler melden</translation>
+    </message>
+</context>
+</TS>
