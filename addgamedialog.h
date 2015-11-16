91f6adaa27ec572d877b521b5fe809cccd24bf91
Florian Bury <dev@florian-bury.de>
2015-11-16 21:46:21 +0100

    [ISSUE 6]
    Added Documentation

--- addgamedialog.h
+++ addgamedialog.h
@@ -1,3 +1,13 @@
+/**
+ * @class AddGameDialog
+ *
+ * @brief A Dialog to add Executables
+ *
+ * This Dialog allows the user to add Executables to his library.
+ *
+ */
+
+
 #ifndef ADDGAMEDIALOG_H
 #define ADDGAMEDIALOG_H
 
@@ -13,6 +23,13 @@ class AddGameDialog : public QDialog
     Q_OBJECT
 
 signals:
+    /**
+     * @fn gameSet
+     * @brief Is used to signal the MainWindow to add the game
+     *
+     * @param game Game, which will be added to the Database.
+     *
+     */
     void gameSet(FGame game);
 
 public:
@@ -20,19 +37,49 @@ public:
     ~AddGameDialog();
 
 private slots:
+
+    /**
+     * @fn on_chooseGameDirButton_clicked
+     * @brief Fires, when the user clicks the Button to select the Game-Directory
+     *
+     */
     void on_chooseGameDirButton_clicked();
+
+    /**
+     * @fn on_chooseGameExecutableButton_clicked
+     * @brief Fires, when the user clicks the Button to select the Game-Executable
+     *
+     */
     void on_chooseGameExecutableButton_clicked();
+
+    /**
+     * @fn on_buttonBox_accepted
+     * @brief Fires, when the user clicks the OK-Button
+     *
+     */
     void on_buttonBox_accepted();
 
+
+    /**
+     * @fn on_gameCommandBrowse_clicked
+     * @brief Fires, when the user clicks the Button to select a Launcher
+     *
+     */
     void on_gameCommandBrowse_clicked();
 
+
+    /**
+     * @fn on_chooseGameDirButton_clicked
+     * @brief Fires, when the user clicks the Button to select the Game-Directory
+     *
+     */
     void on_launcherEnabledCheckBox_clicked();
 
 private:
-    Ui::AddGameDialog *ui;
-    FGame game;
-    bool launchersAvailable;
-    QList<FLauncher>*launchers;
+    Ui::AddGameDialog *ui; //!< Reference to the GUI.
+    FGame game; //!< FGame-Template which is used to collect Information about the new Game.
+    bool launchersAvailable; //!< Flag to check if the game will use a Launcher.
+    QList<FLauncher>*launchers; //!< List of available Launcher.
 };
 
 #endif // ADDGAMEDIALOG_H
