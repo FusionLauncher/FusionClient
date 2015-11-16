c99cc06a8a725b338c7151940c3f1b7b3615a97a
Florian Bury <dev@florian-bury.de>
2015-11-16 22:08:24 +0100

    [ISSUE 8]
    Added Documentation

--- fgamewidget.h
+++ fgamewidget.h
@@ -1,3 +1,11 @@
+/**
+ * @class FGameWidget
+ *
+ * @brief This Widget is displayed in the Main GUI as a List-Entry
+ *
+ *
+ */
+
 #ifndef FGAMEWIDGET_H
 #define FGAMEWIDGET_H
 
@@ -19,8 +27,33 @@ public:
     explicit FGameWidget(QWidget *parent = 0);
     ~FGameWidget();
 
+    /**
+     * @fn setGame
+     * @brief Set the reference to the game which will be displayed
+     *
+     * @param game Game, which will be displayed
+     */
     void setGame(FGame *g);
+
+    /**
+     * @fn setActive
+     * @brief This function is used to create en optical "activated"-effect
+     *
+     * This effect is archied by settings a CSS-property. The actual
+     * color or effect is defined in the stylesheet.
+     *
+     * @param state True/False as in is active / not active
+     *
+     * @todo This needs to be implemented in the Stylesheet
+     */
     void setActive(bool state);
+
+    /**
+     * @fn setFontSize
+     * @brief Used to set the Font-Size of the Game-Title
+     *
+     * @param size Size in PT of the font-Size
+     */
     void setFontSize(int size);
 private:
     Ui::FGameWidget *ui;
@@ -33,6 +66,13 @@ private:
     QGraphicsScene* sceneCover;
     QGraphicsPixmapItem* itemCover;
 
+    /**
+     * @fn updateProperty
+     * @brief This is used to actually set or remove a CSS-Property
+     *
+     * @todo Merge this into the setActive-Function, since we use it only there.
+     *
+     */
     void updateProperty(QWidget *obj, const char *prop, const QVariant value);
 protected:
     void mousePressEvent(QMouseEvent *event);
